a55a1354cb8869f3222f893b4a666f7fff29953d
FiddleMe <bryenyeoh1@gmail.com>
2023-03-14 10:38:51 +0800

    push

---
+++ microservices-parent/product-service/src/main/java/com/ProductMicroservice/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ProductMicroservice.productservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ProductMicroservice.productservice.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
--- microservices-parent/product-service/src/main/java/com/ProductMicroservice/productservice/service/ProductService.java
+++ microservices-parent/product-service/src/main/java/com/ProductMicroservice/productservice/service/ProductService.java
@@ -2,12 +2,16 @@ package com.ProductMicroservice.productservice.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.ProductMicroservice.productservice.dto.ProductRequest;
 import com.ProductMicroservice.productservice.dto.ProductResponse;
 import com.ProductMicroservice.productservice.model.Product;
-import com.ProductMicroservice.productservice.model.repository.ProductRepository;
+import com.ProductMicroservice.productservice.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.getName())
@@ -27,11 +33,14 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+
     }
 
-    public List<ProductResponse> getAllProducts(){
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
-        products.stream().map(product -> mapToProductResponse())
+        return products.stream().map(this::mapToProductResponse).toList();
     }
 
     private ProductResponse mapToProductResponse(Product product) {
