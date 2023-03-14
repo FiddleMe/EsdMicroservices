b517463735e0f40cb12b06825e8267f697edefb1
FiddleMe <bryenyeoh1@gmail.com>
2023-03-14 09:50:59 +0800

    push

--- product-service/src/main/java/com/ProductMicroservice/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ProductMicroservice.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.ProductMicroservice.productservice.model.Product;
-public interface ProductRepository extends MongoRepository<Product, String> {
-    
-}
