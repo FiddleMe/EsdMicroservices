5064793aa942fa8a39fca64a078aec5f0994f2a5
Tiffanyy01 <tiffanyhujiahui@gmail.com>
2023-03-19 00:26:03 +0800

    new

--- signup_login/Dockerfile/dockerfile.sql
+++ signup_login/Dockerfile/dockerfile.sql
@@ -1,5 +1,5 @@
-FROM node:18.15.0-alpine
-WORKDIR /app
-COPY . /app
-RUN npm install
-CMD [ "npm", "start" ]
+#node:18.15.0-alpine
+#WORKDIR /app
+#COPY . /app
+#RUN npm install
+#CMD [ "npm", "start" ]
--- signup_login/userdetails-microservice/userdetails.Dockerfile
+++ signup_login/userdetails-microservice/userdetails.Dockerfile
@@ -1,6 +1,6 @@
 FROM node:18.15.0-alpine
 WORKDIR /app
-COPY . /app
+COPY C:\Users\tiffa\Documents\GitHub\EsdMicroservices\signup_login
 RUN npm install
 EXPOSE 5000
 CMD [ "npm", "start" ]
