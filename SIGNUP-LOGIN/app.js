29d6684cc2164be48a6996b7c9eb1b7a52ffb86e
Tiffanyy01 <tiffanyhujiahui@gmail.com>
2023-03-22 15:30:36 +0800

    new

--- SIGNUP-LOGIN/app.js
+++ SIGNUP-LOGIN/app.js
@@ -9,9 +9,17 @@ app.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+    const userCredential = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
     const user = userCredential.user;
-    res.status(201).json({ message: "User account created", data: { uid: user.uid, email: user.email } });
+    res
+      .status(201)
+      .json({
+        message: "User account created",
+        data: { uid: user.uid, email: user.email },
+      });
   } catch (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -23,9 +31,14 @@ app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const userCredential = await auth.signInWithEmailAndPassword(email, password);
+    const userCredential = await auth.signInWithEmailAndPassword(
+      email,
+      password
+    );
     const user = userCredential.user;
-    res.status(200).json({ message: "Login successful", data: { uid: user.uid, email: user.email } });
+    res
+      .status(200)
+      .json({ message: "Login successful", data: { uid: user.uid, email: user.email } });
   } catch (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -43,4 +56,8 @@ app.post("/logout", (req, res) => {
       const errorMessage = error.message;
       res.status(500).json({ message: errorMessage });
     });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
--- SIGNUP-LOGIN/firebase.js
+++ SIGNUP-LOGIN/firebase.js
@@ -1,18 +1,17 @@
-const { initializeApp } = require("firebase/app");
-const { getAuth } = require("firebase/auth");
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB7rDn1qvBFYyVuw_Pi-1v3l8m8PD2LcZo",
-    authDomain: "esd-signup-login.firebaseapp.com",
+    authDomain: "esd-signfirebaseup-login.firebaseapp.com",
     projectId: "esd-signup-login",
     storageBucket: "esd-signup-login.appspot.com",
     messagingSenderId: "722843795622",
     appId: "1:722843795622:web:47277a9417211e9d1ab467",
     measurementId: "G-285QJG0C38"
 };
+firebase.initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig);
 
-const auth = getAuth();
-
-module.exports = auth;
+const auth = firebase.auth();
+module.exports = firebase.auth();
