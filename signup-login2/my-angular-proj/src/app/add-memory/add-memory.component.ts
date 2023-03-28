caaeaed8b414891a2ff592d3a90e894b02a4af0f
Tiffanyy01 <tiffanyhujiahui@gmail.com>
2023-03-28 08:52:31 +0800

    new

--- signup-login2/my-angular-proj/src/app/add-memory/add-memory.component.ts
+++ signup-login2/my-angular-proj/src/app/add-memory/add-memory.component.ts
@@ -1,21 +1,24 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
- selector: 'app-add-memory',
- templateUrl: './add-memory.component.html',
- styleUrls: ['./add-memory.component.scss']
+  selector: 'app-add-memory',
+  templateUrl: './add-memory.component.html',
+  styleUrls: ['./add-memory.component.scss']
 })
 export class AddMemoryComponent implements OnInit {
- 
+
   memory: any = {};
- 
-  constructor() { }
- 
+
+  constructor(private db: AngularFireDatabase) { }
+
   ngOnInit() {
- 
+
   }
- 
+
   onSubmit() {
-    console.log(this.memory);
+    this.memory.date = new Date(this.memory.date).valueOf();    
+    this.db.list('memories').push(this.memory)
+      .then(_ => console.log('success'))      
   }
 }
